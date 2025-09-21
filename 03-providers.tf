0c11de76927a256ce7f9fcec12fdf0facf731a91
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2025-09-21 15:16:53 -0400

    .

--- 03-providers.tf
+++ 03-providers.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
-  endpoint  = "https://var.PROXMOX_ENDPOINT/"
+  endpoint  = "https://${var.PROXMOX_ENDPOINT}/"
   api_token = "${var.PROXMOX_TOKEN_ID}@PVE!TOKENID=${var.PROXMOX_TOKEN_SECRET}"
   insecure  = true
 }

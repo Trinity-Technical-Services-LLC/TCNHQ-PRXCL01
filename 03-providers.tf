a649c7dacf04fd15a5041c1c736fd18782aad3e7
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2025-09-21 15:10:39 -0400

    Fixed variable name types in 03-providers.tf

--- 03-providers.tf
+++ 03-providers.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
   endpoint  = var.PROXMOX_ENDPOINT
-  api_token = "${var.PROXMOX_API_TOKEN_ID}=${var.PROXMOX_API_TOKEN_SECRET}"
+  api_token = "${var.PROXMOX_TOKEN_ID}=${var.PROXMOX_TOKEN_SECRET}"
   insecure  = true
 }

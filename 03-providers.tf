122be7a8402487c92262bc97d0d61729981ccc7b
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2025-09-21 15:09:51 -0400

    modified:   03-providers.tf

--- 03-providers.tf
+++ 03-providers.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
-  endpoint   = var.PROXMOX_ENDPOINT
-  api_token  = "${var.PROXMOX_API_TOKEN_ID}=${var.PROXMOX_API_TOKEN_SECRET}"
-  verify_tls = var.PROXMOX_VERIFY_TLS
+  endpoint  = var.PROXMOX_ENDPOINT
+  api_token = "${var.PROXMOX_API_TOKEN_ID}=${var.PROXMOX_API_TOKEN_SECRET}"
+  insecure  = true
 }

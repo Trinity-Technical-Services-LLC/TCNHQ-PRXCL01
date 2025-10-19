8bbb2a334e69ad74fa8697c6d0b9594ef9d566e8
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2025-10-18 20:34:21 -0400

    Changes to be committed: 	modified:   locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
-  
+
   all_proxmox_pools = {
-    for pool in var.all_proxmoxvar.all_proxmox_pools : pool.pool_id => {
+    for pool in var.all_proxmox_pools : pool.pool_id => {
       pool_id = pool.pool_id
       comment = pool.comment
     }
